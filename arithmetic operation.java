1ebde4abe8cf95c45db2326c31e8b8b2fb2c6587
anitha111 <43057603+anitha111@users.noreply.github.com>
2018-09-07 11:37:16 +0530

    Add files via upload

---
+++ arithmetic operation.java
@@ -0,0 +1,15 @@
+class Main {
+  public static void main(String[] args) {
+    int a,b,c,d,e,f;
+    b=5;
+    c=3;
+    a=b-c;
+    d=b+c;
+    e=b*c;
+    f=b/c;
+    System.out.println("substract two integers"+a);
+    System.out.println("sum of two integers"+d);
+    System.out.println("multiplication of two integers"+e);
+    System.out.println("division of two integers"+f);
+  }
+}
