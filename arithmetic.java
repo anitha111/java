9d59fe0260155b05bb264e116a79a05a7c1e347c
anitha111 <43057603+anitha111@users.noreply.github.com>
2018-09-12 15:49:51 +0530

    Add files via upload

---
+++ arithmetic.java
@@ -0,0 +1,53 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GUVI
+{
+	public static void main (String[] args) {
+	int(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q);
+       a=3;
+       b=6;
+       c=3;
+       d=7;
+       e=1;
+       f=7;
+       g=8;
+       h=6;
+       i=8;
+       j=6;
+       k=8;
+       l=7;
+       m=9;
+       n=9;
+       o=3;
+       p=3;
+       c=a+b
+       d=a-b
+       e=a*b
+       f=b/a
+       g=b%a
+       h=b++
+       i=b—
+       j=a&b
+       k=a|b
+       l=a^b
+       m=~a
+       n=a<<2
+       o=>>2
+       p=a>>>2
+       system.out.println("output is "+c)
+       system.out.println("output is "+d)
+       system.out.println("output is "+e)
+       system.out.println("output is "+f)
+       system.out.println("output is "+g)
+       system.out.println("output is "+h)
+       system.out.println("output is "+i)
+       system.out.println("output is "+j)
+       system.out.println("output is "+k)
+       system.out.println("output is "+l)
+       system.out.println("output is "+m)
+       system.out.println("output is "+n)
+       system.out.println("output is "+o)
+       system.out.println("output is "+p)
+}    
+}                        
