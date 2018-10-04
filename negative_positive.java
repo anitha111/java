350ccc782c261d454057418026cdab814bc20ec6
anitha111 <43057603+anitha111@users.noreply.github.com>
2018-10-04 12:41:33 +0530

    Update and rename negative positive.java to negative_positive.java

---
+++ negative_positive.java
@@ -0,0 +1,13 @@
+class negative_positive {
+  public static void main(String[] args) {
+  int num =109;
+  if (num>0)
+System.out.println(number+ "positive");
+else if (num <0)
+System.out.println(num+"negative");
+else if (num <= 0)
+System.out.println(num+"zero");
+else
+System.out.println(num+"is a neither positive or negative or zero");
+}
+}
