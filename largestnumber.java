2a80376eeae13f04cbf1b92069ccd0b4a369497a
anitha111 <43057603+anitha111@users.noreply.github.com>
2018-10-05 12:21:38 +0530

    Add files via upload

---
+++ largestnumber.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        double n1 = 2, n2 = 5, n3 = 9;
+
+        if( n1 >= n2 && n1 >= n3)
+            System.out.println(n1 + " is the largest number.");
+
+        else if (n2 >= n1 && n2 >= n3)
+            System.out.println(n2 + " is the largest number.");
+
+        else
+            System.out.println(n3 + " is the largest number.");
+    }
+}
